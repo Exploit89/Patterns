143a81a79c12b889f1de912abb7210071ecd3648
Exploit89 <khudajbergenov89@gmail.com>
2022-06-01 16:54:46 +1000

    PC_Work commit

---
+++ Strategy/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car(new PetrolMove());
+            auto.Move();
+            auto.Movable = new ElectricMove();
+            auto.Move();
+        }
+    }
+
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на бензине");
+        }
+    }
+
+    class ElectricMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на электричестве");
+        }
+    }
+    class Car
+    {
+        public IMovable Movable { private get; set; }
+
+        public Car(IMovable mov)
+        {
+            Movable = mov;
+        }
+
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
---
+++ Strategy/StrategySample.cs
@@ -0,0 +1,31 @@
+﻿public interface IStrategy
+{
+    void Algorithm();
+}
+
+public class ConcreteStrategy1 : IStrategy
+{
+    public void Algorithm()
+    { }
+}
+
+public class ConcreteStrategy2 : IStrategy
+{
+    public void Algorithm()
+    { }
+}
+
+public class Context
+{
+    public IStrategy ContextStrategy { get; set; }
+
+    public Context(IStrategy _strategy)
+    {
+        ContextStrategy = _strategy;
+    }
+
+    public void ExecuteAlgorithm()
+    {
+        ContextStrategy.Algorithm();
+    }
+}
