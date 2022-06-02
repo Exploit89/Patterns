57e0ff453aa555324115e303e16fc01a41bba457
Exploit89 <khudajbergenov89@gmail.com>
2022-06-02 13:18:36 +1000

    PC_Work commit

---
+++ Mediator/MediatorSample.cs
@@ -0,0 +1,57 @@
+﻿abstract class MediatorP
+{
+    public abstract void Send(string msg, Colleague colleague);
+}
+
+abstract class Colleague
+{
+    protected MediatorP mediator;
+
+    public Colleague(MediatorP mediator)
+    {
+        this.mediator = mediator;
+    }
+}
+
+class ConcreteColleague1 : Colleague
+{
+    public ConcreteColleague1(MediatorP mediator)
+        : base(mediator)
+    { }
+
+    public void Send(string message)
+    {
+        mediator.Send(message, this);
+    }
+
+    public void Notify(string message)
+    { }
+}
+
+class ConcreteColleague2 : Colleague
+{
+    public ConcreteColleague2(MediatorP mediator)
+        : base(mediator)
+    { }
+
+    public void Send(string message)
+    {
+        mediator.Send(message, this);
+    }
+
+    public void Notify(string message)
+    { }
+}
+
+class ConcreteMediator : MediatorP
+{
+    public ConcreteColleague1 Colleague1 { get; set; }
+    public ConcreteColleague2 Colleague2 { get; set; }
+    public override void Send(string msg, Colleague colleague)
+    {
+        if (Colleague1 == colleague)
+            Colleague2.Notify(msg);
+        else
+            Colleague1.Notify(msg);
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Mediator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerMediator mediator = new ManagerMediator();
+            Colleague customer = new CustomerColleague(mediator);
+            Colleague programmer = new ProgrammerColleague(mediator);
+            Colleague tester = new TesterColleague(mediator);
+            mediator.Customer = customer;
+            mediator.Programmer = programmer;
+            mediator.Tester = tester;
+            customer.Send("Есть заказ, надо сделать программу");
+            programmer.Send("Программа готова, надо протестировать");
+            tester.Send("Программа протестирована и готова к продаже");
+
+            Console.Read();
+        }
+    }
+
+    abstract class Mediator
+    {
+        public abstract void Send(string msg, Colleague colleague);
+    }
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public abstract void Notify(string message);
+    }
+    // класс заказчика
+    class CustomerColleague : Colleague
+    {
+        public CustomerColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение заказчику: " + message);
+        }
+    }
+    // класс программиста
+    class ProgrammerColleague : Colleague
+    {
+        public ProgrammerColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение программисту: " + message);
+        }
+    }
+    // класс тестера
+    class TesterColleague : Colleague
+    {
+        public TesterColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение тестеру: " + message);
+        }
+    }
+
+    class ManagerMediator : Mediator
+    {
+        public Colleague Customer { get; set; }
+        public Colleague Programmer { get; set; }
+        public Colleague Tester { get; set; }
+        public override void Send(string msg, Colleague colleague)
+        {
+            // если отправитель - заказчик, значит есть новый заказ
+            // отправляем сообщение программисту - выполнить заказ
+            if (Customer == colleague)
+                Programmer.Notify(msg);
+            // если отправитель - программист, то можно приступать к тестированию
+            // отправляем сообщение тестеру
+            else if (Programmer == colleague)
+                Tester.Notify(msg);
+            // если отправитель - тест, значит продукт готов
+            // отправляем сообщение заказчику
+            else if (Tester == colleague)
+                Customer.Notify(msg);
+        }
+    }
+}
