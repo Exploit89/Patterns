0fe26a6af30d54e6335de4e6e742e06c3d11364c
Exploit89 <khudajbergenov89@gmail.com>
2022-06-03 09:09:35 +1000

    PC_Work commit

---
+++ Iterator/IteratorSample.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+
+class Client
+{
+    public void Main()
+    {
+        Aggregate a = new ConcreteAggregate();
+
+        IteratorP i = a.CreateIterator();
+
+        object item = i.First();
+        while (!i.IsDone())
+        {
+            item = i.Next();
+        }
+    }
+}
+
+abstract class Aggregate
+{
+    public abstract IteratorP CreateIterator();
+    public abstract int Count { get; protected set; }
+    public abstract object this[int index] { get; set; }
+}
+
+class ConcreteAggregate : Aggregate
+{
+    private readonly ArrayList _items = new ArrayList();
+
+    public override IteratorP CreateIterator()
+    {
+        return new ConcreteIterator(this);
+    }
+
+    public override int Count
+    {
+        get { return _items.Count; }
+        protected set { }
+    }
+
+    public override object this[int index]
+    {
+        get { return _items[index]; }
+        set { _items.Insert(index, value); }
+    }
+}
+abstract class IteratorP
+{
+    public abstract object First();
+    public abstract object Next();
+    public abstract bool IsDone();
+    public abstract object CurrentItem();
+}
+
+class ConcreteIterator : IteratorP
+{
+    private readonly Aggregate _aggregate;
+    private int _current;
+
+    public ConcreteIterator(Aggregate aggregate)
+    {
+        this._aggregate = aggregate;
+    }
+
+    public override object First()
+    {
+        return _aggregate[0];
+    }
+
+    public override object Next()
+    {
+        object ret = null;
+
+        _current++;
+
+        if (_current < _aggregate.Count)
+        {
+            ret = _aggregate[_current];
+        }
+
+        return ret;
+    }
+
+    public override object CurrentItem()
+    {
+        return _aggregate[_current];
+    }
+
+    public override bool IsDone()
+    {
+        return _current >= _aggregate.Count;
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            Reader reader = new Reader();
+            reader.SeeBooks(library);
+
+            Console.Read();
+        }
+    }
+
+    class Reader
+    {
+        public void SeeBooks(Library library)
+        {
+            IBookIterator iterator = library.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Book book = iterator.Next();
+                Console.WriteLine(book.Name);
+            }
+        }
+    }
+
+    interface IBookIterator
+    {
+        bool HasNext();
+        Book Next();
+    }
+    interface IBookNumerable
+    {
+        IBookIterator CreateNumerator();
+        int Count { get; }
+        Book this[int index] { get; }
+    }
+    class Book
+    {
+        public string Name { get; set; }
+    }
+
+    class Library : IBookNumerable
+    {
+        private Book[] books;
+        public Library()
+        {
+            books = new Book[]
+            {
+            new Book{Name="Война и мир"},
+            new Book {Name="Отцы и дети"},
+            new Book {Name="Вишневый сад"}
+            };
+        }
+        public int Count
+        {
+            get { return books.Length; }
+        }
+
+        public Book this[int index]
+        {
+            get { return books[index]; }
+        }
+        public IBookIterator CreateNumerator()
+        {
+            return new LibraryNumerator(this);
+        }
+    }
+    class LibraryNumerator : IBookIterator
+    {
+        IBookNumerable aggregate;
+        int index = 0;
+        public LibraryNumerator(IBookNumerable a)
+        {
+            aggregate = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
