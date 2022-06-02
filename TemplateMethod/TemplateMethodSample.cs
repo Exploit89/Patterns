ae03b12cbebf49476c28db8fe194f14fa2f1b708
Exploit89 <khudajbergenov89@gmail.com>
2022-06-02 11:35:46 +1000

    PC_Work commit

---
+++ TemplateMethod/TemplateMethodSample.cs
@@ -0,0 +1,21 @@
+﻿abstract class AbstractClass
+{
+    public void TemplateMethod()
+    {
+        Operation1();
+        Operation2();
+    }
+    public abstract void Operation1();
+    public abstract void Operation2();
+}
+
+class ConcreteClass : AbstractClass
+{
+    public override void Operation1()
+    {
+    }
+
+    public override void Operation2()
+    {
+    }
+}
