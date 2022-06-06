f5cb3acc1da6e5276ce3cc0f3c1c9e4b50180aad
Exploit89 <khudajbergenov89@gmail.com>
2022-06-06 12:07:46 +1000

    PC_Work commit

---
+++ Observer/ObserverSample.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+interface IObservable
+{
+    void AddObserver(IObserver o);
+    void RemoveObserver(IObserver o);
+    void NotifyObservers();
+}
+class ConcreteObservable : IObservable
+{
+    private List<IObserver> observers;
+    public ConcreteObservable()
+    {
+        observers = new List<IObserver>();
+    }
+    public void AddObserver(IObserver o)
+    {
+        observers.Add(o);
+    }
+
+    public void RemoveObserver(IObserver o)
+    {
+        observers.Remove(o);
+    }
+
+    public void NotifyObservers()
+    {
+        foreach (IObserver observer in observers)
+            observer.Update();
+    }
+}
+
+interface IObserver
+{
+    void Update();
+}
+class ConcreteObserver : IObserver
+{
+    public void Update()
+    {
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock stock = new Stock();
+            Bank bank = new Bank("ЮнитБанк", stock);
+            Broker broker = new Broker("Иван Иваныч", stock);
+            // имитация торгов
+            stock.Market();
+            // брокер прекращает наблюдать за торгами
+            broker.StopTrade();
+            // имитация торгов
+            stock.Market();
+
+            Console.Read();
+        }
+    }
+
+    interface IObserver
+    {
+        void Update(Object ob);
+    }
+
+    interface IObservable
+    {
+        void RegisterObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
+    }
+
+    class Stock : IObservable
+    {
+        StockInfo sInfo; // информация о торгах
+
+        List<IObserver> observers;
+        public Stock()
+        {
+            observers = new List<IObserver>();
+            sInfo = new StockInfo();
+        }
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.Update(sInfo);
+            }
+        }
+
+        public void Market()
+        {
+            Random rnd = new Random();
+            sInfo.USD = rnd.Next(20, 40);
+            sInfo.Euro = rnd.Next(30, 50);
+            NotifyObservers();
+        }
+    }
+
+    class StockInfo
+    {
+        public int USD { get; set; }
+        public int Euro { get; set; }
+    }
+
+    class Broker : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+        public Broker(string name, IObservable obs)
+        {
+            this.Name = name;
+            stock = obs;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.USD > 30)
+                Console.WriteLine("Брокер {0} продает доллары;  Курс доллара: {1}", this.Name, sInfo.USD);
+            else
+                Console.WriteLine("Брокер {0} покупает доллары;  Курс доллара: {1}", this.Name, sInfo.USD);
+        }
+        public void StopTrade()
+        {
+            stock.RemoveObserver(this);
+            stock = null;
+        }
+    }
+
+    class Bank : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+        public Bank(string name, IObservable obs)
+        {
+            this.Name = name;
+            stock = obs;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object ob)
+        {
+            StockInfo sInfo = (StockInfo)ob;
+
+            if (sInfo.Euro > 40)
+                Console.WriteLine("Банк {0} продает евро;  Курс евро: {1}", this.Name, sInfo.Euro);
+            else
+                Console.WriteLine("Банк {0} покупает евро;  Курс евро: {1}", this.Name, sInfo.Euro);
+        }
+    }
+}
