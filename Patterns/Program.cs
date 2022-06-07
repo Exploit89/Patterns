f7f501051985a4aeca955cd0cf91c9aef3f3cfd6
Exploit89 <khudajbergenov89@gmail.com>
2022-06-08 09:24:55 +1000

    PC_Work commit

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Patterns
 {
-    internal class Program
+    //Написать функцию, которая запрашивает число у пользователя (с помощью метода Console.ReadLine() )
+    //и пытается сконвертировать его в тип int (с помощью int.TryParse())
+    //Если конвертация не удалась у пользователя запрашивается число повторно до тех пор, пока не будет введено верно.
+    //После ввода, который удалось преобразовать в число, число возвращается.
+
+    //P.S Задача решается с помощью циклов
+
+    //P.S Также в TryParse используется модфикатор параметра out
+
+    internal class Programm
     {
         static void Main(string[] args)
         {
+            int numberToConvert;
+            Convert(out numberToConvert);
+        }
+
+        static void Convert(out int number)
+        {
+            bool isEntering = true;
+            int tempNumber = 0;// ДОБАВИЛ ТУТ
+
+            while (isEntering)
+            {
+                Console.WriteLine("Введите любое число.");
+                bool isConverted = int.TryParse(Console.ReadLine(), out number);
+
+                if (isConverted == true)
+                {
+                    Console.WriteLine("Переконвертация прошла успешно.");
+                    isEntering = false;
+                    tempNumber = number; // ДОБАВИЛ ТУТ
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели некорректное число. Попробуйте снова.");
+                }
+            }
+            number = tempNumber;// ДОБАВИЛ ТУТ
         }
     }
 }
