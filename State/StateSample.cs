7177a2524909c082744029cb80fb3f8b2eff3ee1
Exploit89 <khudajbergenov89@gmail.com>
2022-06-08 11:51:34 +1000

    PC_Work commit

---
+++ State/StateSample.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Context context = new Context(new StateA());
+        context.Request(); // Переход в состояние StateB
+        context.Request();  // Переход в состояние StateA
+    }
+}
+abstract class StateP
+{
+    public abstract void Handle(Context context);
+}
+class StateA : StateP
+{
+    public override void Handle(Context context)
+    {
+        context.State = new StateB();
+    }
+}
+class StateB : StateP
+{
+    public override void Handle(Context context)
+    {
+        context.State = new StateA();
+    }
+}
+
+class Context
+{
+    public StateP State { get; set; }
+    public Context(StateP state)
+    {
+        this.State = state;
+    }
+    public void Request()
+    {
+        this.State.Handle(this);
+    }
+}
