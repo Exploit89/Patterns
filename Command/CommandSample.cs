1484acdfa9d7ef493e40377068e94cad1bf24746
Exploit89 <khudajbergenov89@gmail.com>
2022-06-08 10:26:21 +1000

    PC_Work commit

---
+++ Command/CommandSample.cs
@@ -0,0 +1,56 @@
+﻿abstract class CommandP
+{
+    public abstract void Execute();
+    public abstract void Undo();
+}
+// конкретная команда
+class ConcreteCommand : CommandP
+{
+    Receiver receiver;
+    public ConcreteCommand(Receiver r)
+    {
+        receiver = r;
+    }
+    public override void Execute()
+    {
+        receiver.Operation();
+    }
+
+    public override void Undo()
+    { }
+}
+
+// получатель команды
+class Receiver
+{
+    public void Operation()
+    { }
+}
+// инициатор команды
+class Invoker
+{
+    CommandP command;
+    public void SetCommand(CommandP c)
+    {
+        command = c;
+    }
+    public void Run()
+    {
+        command.Execute();
+    }
+    public void Cancel()
+    {
+        command.Undo();
+    }
+}
+class Client
+{
+    void Main()
+    {
+        Invoker invoker = new Invoker();
+        Receiver receiver = new Receiver();
+        ConcreteCommand command = new ConcreteCommand(receiver);
+        invoker.SetCommand(command);
+        invoker.Run();
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Command
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pult pult = new Pult();
+            TV tv = new TV();
+            pult.SetCommand(new TVOnCommand(tv));
+            pult.PressButton();
+            pult.PressUndo();
+
+            Console.Read();
+        }
+    }
+
+    interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    // Receiver - Получатель
+    class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("Телевизор включен!");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Телевизор выключен...");
+        }
+    }
+
+    class TVOnCommand : ICommand
+    {
+        TV tv;
+        public TVOnCommand(TV tvSet)
+        {
+            tv = tvSet;
+        }
+        public void Execute()
+        {
+            tv.On();
+        }
+        public void Undo()
+        {
+            tv.Off();
+        }
+    }
+
+    // Invoker - инициатор
+    class Pult
+    {
+        ICommand command;
+
+        public Pult() { }
+
+        public void SetCommand(ICommand com)
+        {
+            command = com;
+        }
+
+        public void PressButton()
+        {
+            command.Execute();
+        }
+        public void PressUndo()
+        {
+            command.Undo();
+        }
+    }
+}
