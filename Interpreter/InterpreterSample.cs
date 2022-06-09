56b5de1cf09446f812edabe77153eef2fe7baf23
Exploit89 <khudajbergenov89@gmail.com>
2022-06-09 14:35:48 +1000

    PC_Work commit

---
+++ Interpreter/InterpreterSample.cs
@@ -0,0 +1,37 @@
+﻿class Client
+{
+    void Main()
+    {
+        Context context = new Context();
+
+        var expression = new NonterminalExpression();
+        expression.Interpret(context);
+
+    }
+}
+
+class Context
+{
+}
+
+abstract class AbstractExpression
+{
+    public abstract void Interpret(Context context);
+}
+
+class TerminalExpression : AbstractExpression
+{
+    public override void Interpret(Context context)
+    {
+    }
+}
+
+class NonterminalExpression : AbstractExpression
+{
+    AbstractExpression expression1;
+    AbstractExpression expression2;
+    public override void Interpret(Context context)
+    {
+
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            // определяем набор переменных
+            int x = 5;
+            int y = 8;
+            int z = 2;
+
+            // добавляем переменные в контекст
+            context.SetVariable("x", x);
+            context.SetVariable("y", y);
+            context.SetVariable("z", z);
+            // создаем объект для вычисления выражения x + y - z
+            IExpression expression = new SubtractExpression(
+                new AddExpression(
+                    new NumberExpression("x"), new NumberExpression("y")
+                ),
+                new NumberExpression("z")
+            );
+
+            int result = expression.Interpret(context);
+            Console.WriteLine("результат: {0}", result);
+
+            Console.Read();
+        }
+    }
+
+    class Context
+    {
+        Dictionary<string, int> variables;
+        public Context()
+        {
+            variables = new Dictionary<string, int>();
+        }
+        // получаем значение переменной по ее имени
+        public int GetVariable(string name)
+        {
+            return variables[name];
+        }
+
+        public void SetVariable(string name, int value)
+        {
+            if (variables.ContainsKey(name))
+                variables[name] = value;
+            else
+                variables.Add(name, value);
+        }
+    }
+    // интерфейс интерпретатора
+    interface IExpression
+    {
+        int Interpret(Context context);
+    }
+    // терминальное выражение
+    class NumberExpression : IExpression
+    {
+        string name; // имя переменной
+        public NumberExpression(string variableName)
+        {
+            name = variableName;
+        }
+        public int Interpret(Context context)
+        {
+            return context.GetVariable(name);
+        }
+    }
+    // нетерминальное выражение для сложения
+    class AddExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public AddExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) + rightExpression.Interpret(context);
+        }
+    }
+    // нетерминальное выражение для вычитания
+    class SubtractExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public SubtractExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) - rightExpression.Interpret(context);
+        }
+    }
+}
