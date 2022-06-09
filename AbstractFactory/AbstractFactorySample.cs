58b2cbfb78d3ca86153ff842f25bc83cc3a3212c
Exploit89 <khudajbergenov89@gmail.com>
2022-06-10 09:25:56 +1000

    PC_Work commit

---
+++ AbstractFactory/AbstractFactorySample.cs
@@ -0,0 +1,61 @@
+﻿abstract class AbstractFactoryP
+{
+    public abstract AbstractProductA CreateProductA();
+    public abstract AbstractProductB CreateProductB();
+}
+class ConcreteFactory1 : AbstractFactoryP
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA1();
+    }
+
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB1();
+    }
+}
+class ConcreteFactory2 : AbstractFactoryP
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new ProductA2();
+    }
+
+    public override AbstractProductB CreateProductB()
+    {
+        return new ProductB2();
+    }
+}
+
+abstract class AbstractProductA
+{ }
+
+abstract class AbstractProductB
+{ }
+
+class ProductA1 : AbstractProductA
+{ }
+
+class ProductB1 : AbstractProductB
+{ }
+
+class ProductA2 : AbstractProductA
+{ }
+
+class ProductB2 : AbstractProductB
+{ }
+
+class Client
+{
+    private AbstractProductA abstractProductA;
+    private AbstractProductB abstractProductB;
+
+    public Client(AbstractFactoryP factory)
+    {
+        abstractProductB = factory.CreateProductB();
+        abstractProductA = factory.CreateProductA();
+    }
+    public void Run()
+    { }
+}
