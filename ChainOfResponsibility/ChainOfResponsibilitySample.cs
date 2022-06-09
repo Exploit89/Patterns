5cd908dc7e8685e7ebaec5e92f6c00a135bd619d
Exploit89 <khudajbergenov89@gmail.com>
2022-06-09 14:14:41 +1000

    PC_Work commit

---
+++ ChainOfResponsibility/ChainOfResponsibilitySample.cs
@@ -0,0 +1,51 @@
+﻿class Client
+{
+    void Main()
+    {
+        Handler h1 = new ConcreteHandler1();
+        Handler h2 = new ConcreteHandler2();
+        h1.Successor = h2;
+        h1.HandleRequest(2);
+    }
+}
+abstract class Handler
+{
+    public Handler Successor { get; set; }
+    public abstract void HandleRequest(int condition);
+}
+
+class ConcreteHandler1 : Handler
+{
+    public override void HandleRequest(int condition)
+    {
+        // некоторая обработка запроса
+
+        if (condition == 1)
+        {
+            // завершение выполнения запроса;
+        }
+        // передача запроса дальше по цепи при наличии в ней обработчиков
+        else if (Successor != null)
+        {
+            Successor.HandleRequest(condition);
+        }
+    }
+}
+
+class ConcreteHandler2 : Handler
+{
+    public override void HandleRequest(int condition)
+    {
+        // некоторая обработка запроса
+
+        if (condition == 2)
+        {
+            // завершение выполнения запроса;
+        }
+        // передача запроса дальше по цепи при наличии в ней обработчиков
+        else if (Successor != null)
+        {
+            Successor.HandleRequest(condition);
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver receiver = new Receiver(false, true, true);
+
+            PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+            PaymentHandler moneyPaymentHadler = new MoneyPaymentHandler();
+            PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+            bankPaymentHandler.Successor = paypalPaymentHandler;
+            paypalPaymentHandler.Successor = moneyPaymentHadler;
+
+            bankPaymentHandler.Handle(receiver);
+
+            Console.Read();
+        }
+    }
+
+    class Receiver
+    {
+        // банковские переводы
+        public bool BankTransfer { get; set; }
+        // денежные переводы - WesternUnion, Unistream
+        public bool MoneyTransfer { get; set; }
+        // перевод через PayPal
+        public bool PayPalTransfer { get; set; }
+        public Receiver(bool bt, bool mt, bool ppt)
+        {
+            BankTransfer = bt;
+            MoneyTransfer = mt;
+            PayPalTransfer = ppt;
+        }
+    }
+    abstract class PaymentHandler
+    {
+        public PaymentHandler Successor { get; set; }
+        public abstract void Handle(Receiver receiver);
+    }
+
+    class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.BankTransfer == true)
+                Console.WriteLine("Выполняем банковский перевод");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+
+    class PayPalPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.PayPalTransfer == true)
+                Console.WriteLine("Выполняем перевод через PayPal");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+    // переводы с помощью системы денежных переводов
+    class MoneyPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.MoneyTransfer == true)
+                Console.WriteLine("Выполняем перевод через системы денежных переводов");
+            else if (Successor != null)
+                Successor.Handle(receiver);
+        }
+    }
+}
