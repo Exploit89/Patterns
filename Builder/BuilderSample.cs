a69e77173631da7e457a95099b39cfbf9d16a263
Exploit89 <khudajbergenov89@gmail.com>
2022-06-14 14:17:44 +1000

    PC_Work commit

---
+++ Builder/BuilderSample.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+class Client
+{
+    void Main()
+    {
+        BuilderP builder = new ConcreteBuilder();
+        Director director = new Director(builder);
+        director.Construct();
+        Product product = builder.GetResult();
+    }
+}
+class Director
+{
+    BuilderP builder;
+    public Director(BuilderP builder)
+    {
+        this.builder = builder;
+    }
+    public void Construct()
+    {
+        builder.BuildPartA();
+        builder.BuildPartB();
+        builder.BuildPartC();
+    }
+}
+
+abstract class BuilderP
+{
+    public abstract void BuildPartA();
+    public abstract void BuildPartB();
+    public abstract void BuildPartC();
+    public abstract Product GetResult();
+}
+
+class Product
+{
+    List<object> parts = new List<object>();
+    public void Add(string part)
+    {
+        parts.Add(part);
+    }
+}
+
+class ConcreteBuilder : BuilderP
+{
+    Product product = new Product();
+    public override void BuildPartA()
+    {
+        product.Add("Part A");
+    }
+    public override void BuildPartB()
+    {
+        product.Add("Part B");
+    }
+    public override void BuildPartC()
+    {
+        product.Add("Part C");
+    }
+    public override Product GetResult()
+    {
+        return product;
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // содаем объект пекаря
+            Baker baker = new Baker();
+            // создаем билдер для ржаного хлеба
+            BreadBuilder builder = new RyeBreadBuilder();
+            // выпекаем
+            Bread ryeBread = baker.Bake(builder);
+            Console.WriteLine(ryeBread.ToString());
+            // оздаем билдер для пшеничного хлеба
+            builder = new WheatBreadBuilder();
+            Bread wheatBread = baker.Bake(builder);
+            Console.WriteLine(wheatBread.ToString());
+
+            Console.Read();
+        }
+    }
+
+    //мука
+    class Flour
+    {
+        // какого сорта мука
+        public string Sort { get; set; }
+    }
+    // соль
+    class Salt
+    { }
+    // пищевые добавки
+    class Additives
+    {
+        public string Name { get; set; }
+    }
+
+    class Bread
+    {
+        // мука
+        public Flour Flour { get; set; }
+        // соль
+        public Salt Salt { get; set; }
+        // пищевые добавки
+        public Additives Additives { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (Flour != null)
+                sb.Append(Flour.Sort + "\n");
+            if (Salt != null)
+                sb.Append("Соль \n");
+            if (Additives != null)
+                sb.Append("Добавки: " + Additives.Name + " \n");
+            return sb.ToString();
+        }
+    }
+
+    // абстрактный класс строителя
+    abstract class BreadBuilder
+    {
+        public Bread Bread { get; private set; }
+        public void CreateBread()
+        {
+            Bread = new Bread();
+        }
+        public abstract void SetFlour();
+        public abstract void SetSalt();
+        public abstract void SetAdditives();
+    }
+    // пекарь
+    class Baker
+    {
+        public Bread Bake(BreadBuilder breadBuilder)
+        {
+            breadBuilder.CreateBread();
+            breadBuilder.SetFlour();
+            breadBuilder.SetSalt();
+            breadBuilder.SetAdditives();
+            return breadBuilder.Bread;
+        }
+    }
+    // строитель для ржаного хлеба
+    class RyeBreadBuilder : BreadBuilder
+    {
+        public override void SetFlour()
+        {
+            Bread.Flour = new Flour { Sort = "Ржаная мука 1 сорт" };
+        }
+
+        public override void SetSalt()
+        {
+            Bread.Salt = new Salt();
+        }
+
+        public override void SetAdditives()
+        {
+            // не используется
+        }
+    }
+    // строитель для пшеничного хлеба
+    class WheatBreadBuilder : BreadBuilder
+    {
+        public override void SetFlour()
+        {
+            Bread.Flour = new Flour { Sort = "Пшеничная мука высший сорт" };
+        }
+
+        public override void SetSalt()
+        {
+            Bread.Salt = new Salt();
+        }
+
+        public override void SetAdditives()
+        {
+            Bread.Additives = new Additives { Name = "улучшитель хлебопекарный" };
+        }
+    }
+}
