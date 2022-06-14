572f5a712edc1bbc9f851b059d777e8d2aa7dbe2
Exploit89 <khudajbergenov89@gmail.com>
2022-06-14 11:36:13 +1000

    PC_Work commit

---
+++ FactoryMethod/FactoryMethodSample.cs
@@ -0,0 +1,23 @@
+﻿abstract class Product
+{ }
+
+class ConcreteProductA : Product
+{ }
+
+class ConcreteProductB : Product
+{ }
+
+abstract class Creator
+{
+    public abstract Product FactoryMethod();
+}
+
+class ConcreteCreatorA : Creator
+{
+    public override Product FactoryMethod() { return new ConcreteProductA(); }
+}
+
+class ConcreteCreatorB : Creator
+{
+    public override Product FactoryMethod() { return new ConcreteProductB(); }
+}
