0048676d9640ef4698ec4dab620df54b92d01d33
Exploit89 <khudajbergenov89@gmail.com>
2022-06-15 09:09:55 +1000

    PC_Work commit

---
+++ Adapter/AdapterSample.cs
@@ -0,0 +1,31 @@
+﻿class Client
+{
+    public void Request(Target target)
+    {
+        target.Request();
+    }
+}
+// класс, к которому надо адаптировать другой класс   
+class Target
+{
+    public virtual void Request()
+    { }
+}
+
+// Адаптер
+class AdapterP : Target
+{
+    private Adaptee adaptee = new Adaptee();
+
+    public override void Request()
+    {
+        adaptee.SpecificRequest();
+    }
+}
+
+// Адаптируемый класс
+class Adaptee
+{
+    public void SpecificRequest()
+    { }
+}
