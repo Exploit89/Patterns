9597f97185c2e07ccd1fc6439995cea418781bc3
Exploit89 <khudajbergenov89@gmail.com>
2022-06-15 10:26:38 +1000

    PC_Work commit

---
+++ Facade/FacadeSample.cs
@@ -0,0 +1,51 @@
+﻿class Client
+{
+    public void Main(string[] args)
+    {
+        FacadeP facade = new FacadeP(new SubsystemA(), new SubsystemB(), new SubsystemC());
+        facade.Operation1();
+        facade.Operation2();
+    }
+}
+
+class SubsystemA
+{
+    public void A1()
+    { }
+}
+class SubsystemB
+{
+    public void B1()
+    { }
+}
+class SubsystemC
+{
+    public void C1()
+    { }
+}
+
+class FacadeP
+{
+    SubsystemA subsystemA;
+    SubsystemB subsystemB;
+    SubsystemC subsystemC;
+
+    public FacadeP(SubsystemA sa, SubsystemB sb, SubsystemC sc)
+    {
+        subsystemA = sa;
+        subsystemB = sb;
+        subsystemC = sc;
+    }
+    public void Operation1()
+    {
+        subsystemA.A1();
+        subsystemB.B1();
+        subsystemC.C1();
+    }
+    public void Operation2()
+    {
+        subsystemB.B1();
+        subsystemC.C1();
+    }
+}
+
---
+++ Facade/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextEditor textEditor = new TextEditor();
+            Compiller compiller = new Compiller();
+            CLR clr = new CLR();
+
+            VisualStudioFacade ide = new VisualStudioFacade(textEditor, compiller, clr);
+
+            Programmer programmer = new Programmer();
+            programmer.CreateApplication(ide);
+
+            Console.Read();
+        }
+    }
+    // текстовый редактор
+    class TextEditor
+    {
+        public void CreateCode()
+        {
+            Console.WriteLine("Написание кода");
+        }
+        public void Save()
+        {
+            Console.WriteLine("Сохранение кода");
+        }
+    }
+    class Compiller
+    {
+        public void Compile()
+        {
+            Console.WriteLine("Компиляция приложения");
+        }
+    }
+    class CLR
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Выполнение приложения");
+        }
+        public void Finish()
+        {
+            Console.WriteLine("Завершение работы приложения");
+        }
+    }
+
+    class VisualStudioFacade
+    {
+        TextEditor textEditor;
+        Compiller compiller;
+        CLR clr;
+        public VisualStudioFacade(TextEditor te, Compiller compil, CLR clr)
+        {
+            this.textEditor = te;
+            this.compiller = compil;
+            this.clr = clr;
+        }
+        public void Start()
+        {
+            textEditor.CreateCode();
+            textEditor.Save();
+            compiller.Compile();
+            clr.Execute();
+        }
+        public void Stop()
+        {
+            clr.Finish();
+        }
+    }
+
+    class Programmer
+    {
+        public void CreateApplication(VisualStudioFacade facade)
+        {
+            facade.Start();
+            facade.Stop();
+        }
+    }
+}
