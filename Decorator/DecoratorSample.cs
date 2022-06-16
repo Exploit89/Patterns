7065e38b427ff639f2319dae9f12df4bf38686a6
Exploit89 <khudajbergenov89@gmail.com>
2022-06-16 11:41:04 +1000

    PC_Work commit

---
+++ Decorator/DecoratorSample.cs
@@ -0,0 +1,38 @@
+﻿abstract class Component
+{
+    public abstract void Operation();
+}
+class ConcreteComponent : Component
+{
+    public override void Operation()
+    { }
+}
+abstract class DecoratorP : Component
+{
+    protected Component component;
+
+    public void SetComponent(Component component)
+    {
+        this.component = component;
+    }
+
+    public override void Operation()
+    {
+        if (component != null)
+            component.Operation();
+    }
+}
+class ConcreteDecoratorA : DecoratorP
+{
+    public override void Operation()
+    {
+        base.Operation();
+    }
+}
+class ConcreteDecoratorB : DecoratorP
+{
+    public override void Operation()
+    {
+        base.Operation();
+    }
+}
