ceaae8f184eb39091c4b814731ec6b6f883e34cd
Exploit89 <khudajbergenov89@gmail.com>
2022-06-17 13:57:40 +1000

    PC_Work commit

---
+++ Bridge/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Bridge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем нового программиста, он работает с с++
+            Programmer freelancer = new FreelanceProgrammer(new CPPLanguage());
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+            // пришел новый заказ, но теперь нужен c#
+            freelancer.Language = new CSharpLanguage();
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+
+            Console.Read();
+        }
+    }
+
+    interface ILanguage
+    {
+        void Build();
+        void Execute();
+    }
+
+    class CPPLanguage : ILanguage
+    {
+        public void Build()
+        {
+            Console.WriteLine("С помощью компилятора C++ компилируем программу в бинарный код");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Запускаем исполняемый файл программы");
+        }
+    }
+
+    class CSharpLanguage : ILanguage
+    {
+        public void Build()
+        {
+            Console.WriteLine("С помощью компилятора Roslyn компилируем исходный код в файл exe");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("JIT компилирует программу бинарный код");
+            Console.WriteLine("CLR выполняет скомпилированный бинарный код");
+        }
+    }
+
+    abstract class Programmer
+    {
+        protected ILanguage language;
+        public ILanguage Language
+        {
+            set { language = value; }
+        }
+        public Programmer(ILanguage lang)
+        {
+            language = lang;
+        }
+        public virtual void DoWork()
+        {
+            language.Build();
+            language.Execute();
+        }
+        public abstract void EarnMoney();
+    }
+
+    class FreelanceProgrammer : Programmer
+    {
+        public FreelanceProgrammer(ILanguage lang) : base(lang)
+        {
+        }
+        public override void EarnMoney()
+        {
+            Console.WriteLine("Получаем оплату за выполненный заказ");
+        }
+    }
+    class CorporateProgrammer : Programmer
+    {
+        public CorporateProgrammer(ILanguage lang)
+            : base(lang)
+        {
+        }
+        public override void EarnMoney()
+        {
+            Console.WriteLine("Получаем в конце месяца зарплату");
+        }
+    }
+}
